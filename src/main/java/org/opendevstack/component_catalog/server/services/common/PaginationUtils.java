4ad7bc01cc0ac0feca42a5fd1663be5cea08fc66
Iván Risueño <ivan.risueno_martin.ext@boehringer-ingelheim.com>
2026-06-16 16:37:31 +0200

    Fix little bug on next/previous numbers on URLs

--- src/main/java/org/opendevstack/component_catalog/server/services/common/PaginationUtils.java
+++ src/main/java/org/opendevstack/component_catalog/server/services/common/PaginationUtils.java
@@ -10,8 +10,8 @@ public class PaginationUtils {
     public static Pagination buildPagination(int page, int size, int totalElements, String basePath) {
         int totalPages = (size == 0) ? 0 : (int) Math.ceil((double) totalElements / size);
 
-        var next = (page < totalPages - 1) ? URI.create(basePath + "?page=" + Math.max((page + 1), 0) + "&size=" + size) : null;
-        var previous = (page > 0) ? URI.create(basePath + "?page=" + Math.min((page - 1), totalPages) + "&size=" + size) : null;
+        var next = (page < totalPages - 1) ? URI.create(basePath + "?page=" + Math.max(page + 1, 1) + "&size=" + size) : null;
+        var previous = (page > 0) ? URI.create(basePath + "?page=" + Math.min(page - 1, totalPages - 1) + "&size=" + size) : null;
 
         return Pagination.builder()
                 .page(page)
