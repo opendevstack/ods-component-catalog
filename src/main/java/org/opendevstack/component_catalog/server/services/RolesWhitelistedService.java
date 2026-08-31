086bb731cb366fedc499bf0c3e5074908091d2c1
Iván Risueño <ivan.risueno_martin.ext@boehringer-ingelheim.com>
2026-08-31 15:29:34 +0200

    Add RolesWhitelisted suffixes to GroupsRestrictionsEvaluator

--- src/main/java/org/opendevstack/component_catalog/server/services/RolesWhitelistedService.java
+++ src/main/java/org/opendevstack/component_catalog/server/services/RolesWhitelistedService.java
@@ -1,4 +1,55 @@
 package org.opendevstack.component_catalog.server.services;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.opendevstack.component_catalog.config.ProvisionerActionsConfiguration;
+import org.opendevstack.component_catalog.server.services.bitbucket.BitbucketPathAt;
+import org.opendevstack.component_catalog.server.services.catalog.CatalogServiceAdapter;
+import org.opendevstack.component_catalog.server.services.catalog.entity.RolesWhitelisted;
+import org.opendevstack.component_catalog.server.services.exceptions.InvalidEntityException;
+import org.opendevstack.component_catalog.server.services.exceptions.InvalidIdException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class RolesWhitelistedService {
+
+    private final CatalogServiceAdapter catalogServiceAdapter;
+    private final BitbucketService bitbucketService;
+    private final ProvisionerActionsConfiguration provisionerActionsConfiguration;
+
+    public List<String> resolveWhitelistedRolesForCatalogItemId(String catalogItemId) {
+        BitbucketPathAt catalogItemPathAt;
+        try {
+            catalogItemPathAt = catalogServiceAdapter.bitbucketPathAtFromId(catalogItemId);
+        } catch (InvalidIdException e) {
+            throw new InvalidEntityException("Invalid catalogItemId: %s".formatted(catalogItemId));
+        }
+
+        var itemSlug = catalogItemPathAt.getProjectKey().toLowerCase().concat("_").concat(catalogItemPathAt.getRepoSlug());
+        BitbucketPathAt pathAt = buildRolesWhitelistedBitbucketPathAt();
+
+        Optional<RolesWhitelisted> roles = catalogServiceAdapter.getYamlEntity(pathAt, RolesWhitelisted.class);
+
+        return roles.orElseThrow(() -> new InvalidEntityException("Invalid RolesWhitelisted.yaml file, path: %s".formatted(pathAt.getPathAt())))
+                .getRoles().entrySet().stream()
+                .filter(role -> role.getValue().contains(itemSlug))
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    private BitbucketPathAt buildRolesWhitelistedBitbucketPathAt() {
+        return bitbucketService.pathAtBuilder()
+                .projectKey(provisionerActionsConfiguration.getProjectKey())
+                .repoSlug(provisionerActionsConfiguration.getProjectConfigurationsRepositorySlug())
+                .subPath(provisionerActionsConfiguration.getProjectConfigurationsRolesWhitelistedPath())
+                .at(provisionerActionsConfiguration.getBranchName())
+                .build();
+    }
+
 }
--- src/main/java/org/opendevstack/component_catalog/server/services/restrictions/evaluators/GroupsRestrictionsEvaluator.java
+++ src/main/java/org/opendevstack/component_catalog/server/services/restrictions/evaluators/GroupsRestrictionsEvaluator.java
@@ -1,24 +1,51 @@
 package org.opendevstack.component_catalog.server.services.restrictions.evaluators;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.opendevstack.component_catalog.server.services.RolesWhitelistedService;
 import org.opendevstack.component_catalog.server.services.catalog.common.UserActionEntityRestrictions;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
+import java.util.stream.Stream;
+
 @Service
 @Order(30)
 @Slf4j
+@RequiredArgsConstructor
 public class GroupsRestrictionsEvaluator implements RestrictionsEvaluator {
+
+    private final RolesWhitelistedService rolesWhitelistedService;
 
     @Override
     public RestrictionsEvaluatorResult evaluate(EvaluationRestrictions restrictions, RestrictionsParams params) {
-        if (validate(restrictions.restrictions(), params) && evaluateConditions(restrictions.restrictions(), params)) {
+        UserActionEntityRestrictions updatedRestrictions = addWhitelistedSuffixRolesIfNeeded(restrictions.restrictions(), params.getCatalogItemId());
+        if (validate(updatedRestrictions, params) && evaluateConditions(updatedRestrictions, params)) {
             return new RestrictionsEvaluatorResult(true, true, "");
         } else {
             log.debug("Validation failed for restrictions: {}, and params: {}", restrictions, params);
 
-            return new RestrictionsEvaluatorResult(false, true,"Only project members with Manager or Team roles can provision components.");
+            return new RestrictionsEvaluatorResult(false, true,"Only project members with Manager or Team roles can provision this component.");
         }
+    }
+
+    private UserActionEntityRestrictions addWhitelistedSuffixRolesIfNeeded(UserActionEntityRestrictions restrictions, String catalogItemId) {
+        var whitelistedRoles = rolesWhitelistedService.resolveWhitelistedRolesForCatalogItemId(catalogItemId);
+
+        if (whitelistedRoles.isEmpty()) {
+            log.debug("No whitelisted roles where found for catalog item {}", Base64.getDecoder().decode(catalogItemId));
+            return restrictions;
+        }
+
+        log.debug("Whitelisted roles found for catalog item {}: {}", Base64.getUrlDecoder().decode(catalogItemId), whitelistedRoles);
+        var updatedSuffixes = Stream.concat(restrictions.getGroups().getSuffix().stream(), whitelistedRoles.stream()).distinct().toList();
+
+        return restrictions.toBuilder()
+                .groups(restrictions.getGroups().toBuilder()
+                        .suffix(updatedSuffixes)
+                        .build())
+                .build();
     }
 
     private boolean validate(UserActionEntityRestrictions restrictions, RestrictionsParams params) {
